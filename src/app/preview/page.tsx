75fb911e7c0670040dd629405b53811ad063d18d
bojodiva <ojoabisola387@gmail.com>
2025-04-05 23:52:40 +0100

    deployment fix

---
+++ src/app/preview/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+import { Box, Button, FormControl, FormLabel, Input, Textarea } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
+export default function ResumePreview(){
+    return(
+        <></>
+    )
+}
